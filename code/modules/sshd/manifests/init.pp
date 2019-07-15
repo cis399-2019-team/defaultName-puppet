55aa34644a71a452da2d2ecd313faa206bdcf8e2
Henzi Kou <henzi@Henzis-MacBook-Pro.local>
2019-07-15 14:14:26 -0700

    changes to init.pp

--- code/modules/sshd/manifests/init.pp
+++ code/modules/sshd/manifests/init.pp
@@ -19,7 +19,7 @@ class sshd {
 		ensure	  => running,
 		subscribe => File["/etc/ssh/sshd_config"],
 		require	  => [
-			Package["openshh-server"],
+			Package["openssh-server"],
 		],
 	}
 
@@ -27,7 +27,7 @@ class sshd {
 		ensure => present,
 		user   => "ubuntu",
 		type   => "ssh_rsa",
-		key	   => "AAAAB3NzaC1yc2EAAAADAQABAAABAQCVUbl8/91D5mKl00k8+SRI7vN6m8gYH+NUIUT7vBNhn/RbzfP7z60CaBMX2103/1QdMA1XpLVTbxTC3Is/J3CaI+dpXWmx5kUnK6GCHhCi7UNV1Vkw1nHBgXIDlPlQYZ1iQL/v+DjPUPDRegB09VRd8NGuOtekCI920nq7tDSxnoO+0cH6D82WUGmkBOMXw2G8UdzPTc47NNCbLfBGs0WrxpjEIk/nOZjfrt8DYtJSk8r/Q6XjisAGDdtupjXeTGF5zFj27cmEAwQSmO2IJO1tWX+8WMlNI4awktpSA88V/OGqUuyEpr3NvzzuB69mjW1L689g7rm/+7uUw5Fd62rB"
+		key    => "AAAAB3NzaC1yc2EAAAADAQABAAABAQCVUbl8/91D5mKl00k8+SRI7vN6m8gYH+NUIUT7vBNhn/RbzfP7z60CaBMX2103/1QdMA1XpLVTbxTC3Is/J3CaI+dpXWmx5kUnK6GCHhCi7UNV1Vkw1nHBgXIDlPlQYZ1iQL/v+DjPUPDRegB09VRd8NGuOtekCI920nq7tDSxnoO+0cH6D82WUGmkBOMXw2G8UdzPTc47NNCbLfBGs0WrxpjEIk/nOZjfrt8DYtJSk8r/Q6XjisAGDdtupjXeTGF5zFj27cmEAwQSmO2IJO1tWX+8WMlNI4awktpSA88V/OGqUuyEpr3NvzzuB69mjW1L689g7rm/+7uUw5Fd62rB"
 	}
 
 	ssh_authorized_key { "arobiso2-key-pair":
