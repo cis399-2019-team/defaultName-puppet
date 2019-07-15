93a32a52207a3fab77bffb6a4f776d8482993fbd
root <root@ip-10-0-5-44.us-west-2.compute.internal>
2019-07-15 23:13:52 +0000

    fixed two typos

--- code/modules/sshd/manifests/init.pp
+++ code/modules/sshd/manifests/init.pp
@@ -6,7 +6,7 @@ class sshd {
 
 	file { "/etc/ssh/sshd_config":
 		ensure	=> present,
-		notify	=> Service["sshd"],
+		notify	=> Service["ssh"],
 		mode	=> '444',
 		owner	=> 'root',
 		group	=> 'root',
@@ -26,14 +26,14 @@ class sshd {
 	ssh_authorized_key { "henzik_key":
 		ensure => present,
 		user   => "ubuntu",
-		type   => "ssh_rsa",
+		type   => "ssh-rsa",
 		key    => "AAAAB3NzaC1yc2EAAAADAQABAAABAQCOLytqui3m1l3IQIxcyqQwiWxOZM+IzGYSNpxJ8gnGtYMueYq7OBdayD+ASbbeBexI3YqXGfCdZOhAir6REsBGS9ufVvJn3ljFzcVtMtgfWmwCqODbUtllVfkq8nDoiH5e5U+yPdYDUsuVN6EkczuRMx8M4gMLKFWlgSKM6IFlDWAzcxTvH5DdPZ41Dt2Q+EvszklYAw9ENBMfBfd072wyixYJiPabZfyrjw/PTv1MtIN8OiiB4itWyhpD/olorfX2kVigP8l0SGdeeqFZsm4fx+Vh4BrWJdXMh5PVpWbMjHDH4phDHyPW9g9EPebrOJXK6Rcwr4KGBveIsCjE",
 	}
 
 	ssh_authorized_key { "arobiso2-key-pair":
 		ensure => present,
 		user   => "ubuntu",
-		type   => "ssh_rsa",
+		type   => "ssh-rsa",
 		key    => "AAAAB3NzaC1yc2EAAAADAQABAAABAQCQ56WeuvrBzNa5h1a8j64byA+cmQFACIguAFmLw5IgLkWXFOIuM+faPh/mBM1sUpff10pARcc91BY/3aDa8Tnac0eeXWp5+84JONcZcJxENm2e3pDguWHBDJc8ZmHfHJQGeO+BZvD1Reg4zdS0oC5RfI/iDax+QQ9J02gyPaoKs2C9aPRUjctY/dd+w9bSPv0KMjF2Wu9O5Wrr1zDlos4/zkzgqHrc1+k/7UW4ahLIr6SWO/0xVsJQgeZf2/855109wBCeFXTnB4l4cupc0riovtIUwQeASSCQSuCnrQ1Nc8y0RRusgWIquJZtbdy2IkjSw54rHarnjakI4QLSJ+Wj",		
 	}
 }
