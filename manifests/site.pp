7fb1d3d97254c16f6cf4c840658b56d174d4f132
Henzi Kou <henzi@Henzis-MacBook-Pro.local>
2019-07-21 21:31:55 -0700

    corrected errors

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,11 +1,11 @@
 node ip-10-0-5-201 {
 	include puppet
 	include sshd
-	include apache
+	include apache2
 }
 
 node ip-10-0-5-44 {
 	include puppet
 	include sshd
-	include apache
+	include apache2
 }
