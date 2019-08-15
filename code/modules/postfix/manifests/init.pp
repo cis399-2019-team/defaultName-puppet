48e8ffd246fd411f6bf7cdc08142e10fccb36c20
root <root@ip-10-0-5-201.us-west-2.compute.internal>
2019-08-15 21:22:55 +0000

    update

--- code/modules/postfix/manifests/init.pp
+++ code/modules/postfix/manifests/init.pp
@@ -13,7 +13,8 @@ class postfix {
 		mode       => '444',
 		owner      => 'root',
 		group      => 'root',
-		source     => "puppet:///modules/postfix/main.cf",
-		myhostname => "$ec2-54-190-27-30.us-west-2.compute.amazonaws.com",
+		source     => [
+			"puppet:///modules/postfix/$hostname/main.cf",
+		]
 	}
 }
